27116a8607eb0b19955d250dc21a139cb1e73bd8
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-10-16 22:39:26 -0400

    Create game function

--- app.js
+++ app.js
@@ -14,3 +14,27 @@ function playRound(playerSelection, computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`
     }
 }
+
+function game() {
+    let playerWins = 0;
+    let computerWins = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose: Rock, Paper or Scissors?")
+        const results = playRound(playerSelection, computerPlay());
+        console.log(results)
+        if (results.includes("You Win!")) {
+            playerWins++
+        } else if (results.includes("You Lose!")) {
+            computerWins++;
+        }
+    }
+    if (playerWins > computerWins) {
+        console.log("Player Wins!");
+    } else if (playerWins < computerWins) {
+        console.log("Computer Wins");
+    } else {
+        console.log("It's a tie!");
+    }
+}
+
+game();
