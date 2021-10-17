608591ff30b6e148e80ab6cdd2e923faa8258d45
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-10-16 22:26:45 -0400

    Create a function that plays a single round

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ function computerPlay() {
     const randomIndex = Math.floor(Math.random() * 3);
     return moves[randomIndex];
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1).toLowerCase();
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Paper" && computerSelection === "Rock") || (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`
+    }
+}
