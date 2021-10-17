e881a117291a070b6bd5a4449ea2d93c65b98865
Nakoya Wilson <wilson.nakoya@gmail.com>
2021-10-16 22:45:50 -0400

    Display round number each round

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ function game() {
     for (let i = 0; i < 5; i++) {
         const playerSelection = prompt("Choose: Rock, Paper or Scissors?")
         const results = playRound(playerSelection, computerPlay());
-        console.log(results)
+        console.log(`Round ${i + 1}: ${results}`)
         if (results.includes("You Win!")) {
             playerWins++
         } else if (results.includes("You Lose!")) {
